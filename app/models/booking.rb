d06c2ab7431a019eef35561e731e760632aed725
Sylvain Galtier <sylvaingaltier@gmail.com>
2018-11-16 18:26:11 +0100

    b

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,4 @@ class Booking < ApplicationRecord
   belongs_to :super_hero
   validates :starts_at, presence: true
   validates :ends_at, presence: true
-  validates :rating, inclusion: { in: (0..5) }
-  validates :rating, numericality: { only_integer: true }
 end
